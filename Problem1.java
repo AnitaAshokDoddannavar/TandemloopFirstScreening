eee8c8d71c3efd0a83a3fa3e37e3a2a9c7b28046
AnitaAshokDoddannavar <anitadoddannavar5@gmail.com>
2025-08-07 19:14:39 +0530

    Create Problem1.java

---
+++ Problem1.java
@@ -0,0 +1,49 @@
+package com.tandemloop;
+
+import java.util.Scanner;
+
+public class Problem1 {
+    double a, b;
+    String operation;
+
+    public Problem1(double a, double b, String operation) {
+        this.a = a;
+        this.b = b;
+        this.operation = operation;
+    }
+
+    public void calculate() {
+        switch (operation.toLowerCase()) {
+            case "add":
+                System.out.println("Result: " + (a + b));
+                break;
+            case "subtract":
+                System.out.println("Result: " + (a - b));
+                break;
+            case "multiply":
+                System.out.println("Result: " + (a * b));
+                break;
+            case "divide":
+                if (b != 0) {
+                    System.out.println("Result: " + (a / b));
+                } else {
+                    System.out.println("Cannot divide by zero.");
+                }
+                break;
+            default:
+                System.out.println("Invalid operation.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+        System.out.print("Enter operation (add, subtract, multiply, divide): ");
+        String op = sc.next();
+        Problem1 calc = new Problem1(a, b, op);
+        calc.calculate();
+    }
+}
